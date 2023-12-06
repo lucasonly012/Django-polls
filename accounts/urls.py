6c2f16dcb8aa0c426e488703c80ca0b5f45be025
lucasonly012 <lucaslindo9127@gmail.com>
2023-12-06 10:55:24 -0300

    commits pendentes e atualizados

---
+++ accounts/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from accounts import views
+
+urlpatterns = [
+    path('accounts/signup', # caminho que vai carregar a view com o formulário
+        views.AccountCreateView.as_view(),
+        name="signup"
+    ),
+
+    path('account/<int:pk>/edit',
+        views.AccountUpdateView.as_view(),
+        name="account_edit"
+    ),
+
+    path('account/me',
+        views.AccountTemplateView.as_view(),
+        name="account_detail"
+    ),
+]
